f56bfa403488ea25e245f63bd3ad72a0dcb3d162
Bao <nguyengiabao91753@gmail.com>
2025-10-06 20:41:28 +0700

    Dựng Admin UI

---
+++ RetailShop/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RetailShop.Controllers;
+[Route("login")]
+public class LoginController : Controller
+{
+    [Route("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
