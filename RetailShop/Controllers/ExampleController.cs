c9e4de892d66ebca5db70965c6097316519076ce
Bao <nguyengiabao91753@gmail.com>
2025-10-07 13:45:30 +0700

    UI mẫu

---
+++ RetailShop/Controllers/ExampleController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RetailShop.Controllers;
+[Route("example")]
+public class ExampleController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    [Route("create")]
+    public IActionResult Create()
+    {
+        return View("Create");
+    }
+}
