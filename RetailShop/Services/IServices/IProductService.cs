01271b83b43b5167073c2cb8a6afdc72c9be3c44
HaoWasabi <truonggiahao24@gmail.com>
2025-10-16 23:16:26 +0700

    feat(service): implement product-service for product management

---
+++ RetailShop/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using RetailShop.Dtos;
+using RetailShop.Models;
+
+namespace RetailShop.Services.IServices;
+
+public interface IProductService
+{
+    Task<ResultService<List<Product>>> GetAllProductsAsync();
+    Task<ResultService<Product>> GetProductByIdAsync(int id);
+    Task<ResultService<Product>> CreateProductAsync(Product supplier);
+    Task<ResultService<Product>> UpdateProductAsync(Product supplier);
+    Task<ResultService<Product>> DeleteProductAsync(int id);
+}
---
+++ RetailShop/Services/ProductService.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailShop.Data;
+using RetailShop.Dtos;
+using RetailShop.Models;
+using RetailShop.Services.IServices;
+
+namespace RetailShop.Services;
+
+public class ProductService : IProductService
+{
+    private readonly AppDbContext _db;
+    public ProductService(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<ResultService<Product>> CreateProductAsync(Product product)
+    {
+        var rs = new ResultService<Product>();
+        try
+        {
+            await _db.Products.AddAsync(product);
+            await _db.SaveChangesAsync();
+            rs.IsSuccess = true;
+            rs.Data = product;
+            rs.Message = "Product created successfully.";
+        }
+        catch (Exception ex)
+        {
+            rs.IsSuccess = false;
+            rs.Message = $"Error creating product: {ex.Message}";
+        }
+        return rs;
+    }
+    public async Task<ResultService<List<Product>>> GetAllProductsAsync()
+    {
+        var rs = new ResultService<List<Product>>();
+        try
+        {
+            var products = await _db.Products.ToListAsync();
+            rs.IsSuccess = true;
+            rs.Data = products;
+            rs.Message = "Products retrieved successfully.";
+        }
+        catch (Exception ex)
+        {
+            rs.IsSuccess = false;
+            rs.Message = $"Error retrieving products: {ex.Message}";
+        }
+        return rs;
+    }
+
+    public async Task<ResultService<Product>> GetProductByIdAsync(int id)
+    {
+        var rs = new ResultService<Product>();
+        try
+        {
+            var product = await _db.Products
+                .Include(s => s.Category)
+                .Include(s => s.Supplier)
+                .Include(s => s.Inventories)
+                .Include(s => s.OrderItems)
+            .FirstOrDefaultAsync(s => s.ProductId == id);
+            if (product == null)
+            {
+                rs.IsSuccess = false;
+                rs.Message = "Product not found.";
+            }
+            else
+            {
+                rs.IsSuccess = true;
+                rs.Data = product;
+                rs.Message = "Product retrieved successfully.";
+            }
+        }
+        catch (Exception ex)
+        {
+            rs.IsSuccess = false;
+            rs.Message = $"Error when finding product: {ex.Message}";
+        }
+        return rs;
+    }
+
+    public async Task<ResultService<Product>> UpdateProductAsync(Product product)
+    {
+        var rs = new ResultService<Product>();
+        try
+        {
+            var existingProduct = await _db.Products.FindAsync(product.ProductId);
+            if (existingProduct == null)
+            {
+                rs.IsSuccess = false;
+                rs.Message = "Product not found.";
+                return rs;
+            }
+            existingProduct.ProductName = product.ProductName;
+            existingProduct.CategoryId = product.CategoryId;
+            existingProduct.SupplierId = product.SupplierId;
+            existingProduct.ProductImage = product.ProductImage;
+            existingProduct.Barcode = product.Barcode;
+            existingProduct.Price = product.Price;
+            existingProduct.Unit = product.Unit;
+            existingProduct.CreatedAt = product.CreatedAt;
+
+
+            _db.Products.Update(existingProduct);
+            _db.SaveChanges();
+            rs.IsSuccess = true;
+            rs.Data = existingProduct;
+            rs.Message = "Product updated successfully.";
+        }
+        catch (Exception ex)
+        {
+            rs.IsSuccess = false;
+            rs.Message = $"Error updating product: {ex.Message}";
+        }
+        return rs;
+    }
+
+    public async Task<ResultService<Product>> DeleteProductAsync(int id)
+    {
+        var rs = new ResultService<Product>();
+        try
+        {
+            var existingProduct = await _db.Products.FindAsync(id);
+            if (existingProduct == null)
+            {
+                rs.IsSuccess = false;
+                rs.Message = "Product not found.";
+                return rs;
+            }
+
+            _db.Products.ExecuteDeleteAsync(existingProduct);
+            _db.SaveChanges();
+            rs.IsSuccess = true;
+            rs.Data = existingProduct;
+            rs.Message = "Product deleted successfully.";
+        }
+        catch (Exception ex)
+        {
+            rs.IsSuccess = false;
+            rs.Message = $"Error deleting product: {ex.Message}";
+        }
+        return rs;
+    }
+}
