ce76384a7e195b39bf591e07d0976a0a2e910273
Phuong Anh Nguyen <alixblu3011@gmail.com>
2025-10-19 00:21:01 +0700

    feat: edit promo

--- RetailShop/Controllers/PromotionController.cs
+++ RetailShop/Controllers/PromotionController.cs
@@ -9,11 +9,11 @@ public class PromotionController : Controller
 	private readonly IPromotionService _promotionService;
 
 	public PromotionController(IPromotionService promotionService)
-    {
+	{
 		_promotionService = promotionService;
-    }
+	}
 
-    public async Task<IActionResult> Index()
+	public async Task<IActionResult> Index()
 	{
 		var rs = await _promotionService.GetAllPromotionsAsync();
 		if (rs.IsSuccess)
@@ -25,7 +25,7 @@ public class PromotionController : Controller
 			TempData["err"] = "Lấy danh sách khuyến mãi thất bại: " + rs.Message;
 			ViewBag.Promotions = new List<Promotion>();
 		}
-        return View();
+		return View();
 	}
 
 	[HttpGet]
@@ -33,7 +33,7 @@ public class PromotionController : Controller
 	public IActionResult Create()
 	{
 		return View("Create");
-    }
+	}
 	[HttpPost]
 	[Route("store")]
 	public async Task<IActionResult> Store(Promotion promotion)
@@ -55,5 +55,43 @@ public class PromotionController : Controller
 			TempData["err"] = "Thêm thất bại: " + result.Message;
 			return View("Create", promotion);
 		}
-    }
+	}
+
+	[HttpGet]
+	[Route("edit/{id}")]
+	public async Task<IActionResult> Edit(int id)
+	{
+		var rs = await _promotionService.GetPromotionByIdAsync(id);
+		if (rs.IsSuccess)
+		{
+			return View("Edit", rs.Data);
+		}
+		TempData["err"] = "Lấy mã giảm giá thất bại" + rs.Message;
+		return RedirectToAction("Index");
+	}
+
+	[HttpPost]
+	[Route("update")]
+	public async Task<IActionResult> Update(Promotion promotion)
+	{
+		if (!ModelState.IsValid)
+		{
+			var errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+			TempData["err"] = "Cập nhật thất bại: " + string.Join(", ", errors);
+			return View("Edit", promotion);
+		}
+		var result = await _promotionService.UpdatePromotionAsync(promotion);
+		if (result.IsSuccess)
+		{
+			TempData["success"] = "Cập nhật thành công";
+			return RedirectToAction("Index");
+		}
+		else
+		{
+			TempData["err"] = "Cập nhật thất bại: " + result.Message;
+			return View("Edit", promotion);
+		}
+	}
+	
 }
+
