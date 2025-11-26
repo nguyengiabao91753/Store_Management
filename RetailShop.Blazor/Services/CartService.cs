e0586284cb00a70518ead1e9aed953480d2931cb
Bao <nguyengiabao91753@gmail.com>
2025-11-26 23:56:25 +0700

    Cart Service

---
+++ RetailShop.Blazor/Services/CartService.cs
@@ -0,0 +1,63 @@
+﻿using RetailShop.Blazor.Data;
+using RetailShop.Blazor.Models;
+using RetailShop.Blazor.Services.IServices;
+
+namespace RetailShop.Blazor.Services;
+
+public class CartService : ICartService
+{
+    private readonly AppDbContext _db;
+
+    public CartService(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public bool AddToCart(int product, int quantity)
+    {
+        try {          
+            var existingCartItem = _db.Carts.FirstOrDefault(c => c.ProductId == product);
+            if (existingCartItem != null)
+            {
+                existingCartItem.Quantity += quantity;
+                _db.Carts.Update(existingCartItem);
+            }
+            else
+            {
+                Cart cart = new Cart
+                {
+                    ProductId = product,
+                    Quantity = quantity,
+                    CreatedAt = DateTime.Now
+                };
+                _db.Carts.Add(cart);
+            }
+            _db.SaveChanges();
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public bool ClearCart(List<int> products)
+    {
+        try
+        {
+            var cartItems = _db.Carts.Where(c => products.Contains(c.ProductId)).ToList();
+            _db.Carts.RemoveRange(cartItems);
+            _db.SaveChanges();
+            return true;
+        }catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public List<Cart> GetCart()
+    {
+        var cartItems = _db.Carts.ToList();
+        return cartItems;
+    }
+}
---
+++ RetailShop.Blazor/Services/IServices/ICartService.cs
@@ -0,0 +1,12 @@
+﻿using RetailShop.Blazor.Models;
+
+namespace RetailShop.Blazor.Services.IServices;
+
+public interface ICartService
+{
+    public List<Cart> GetCart();
+
+    public bool AddToCart(int product, int quantity);
+
+    public bool ClearCart(List<int> products);
+}
